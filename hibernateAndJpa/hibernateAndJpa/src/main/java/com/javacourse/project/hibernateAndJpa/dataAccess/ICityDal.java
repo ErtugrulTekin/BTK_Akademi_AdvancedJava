c60273f9c0c468ebad4757fe3b54c1102214bb2c
Ertugrul Tekin <ertugrultekin98@gmail.com>
2022-11-11 20:51:20 +0300

    Advanced Java completed

---
+++ hibernateAndJpa/hibernateAndJpa/src/main/java/com/javacourse/project/hibernateAndJpa/dataAccess/ICityDal.java
@@ -0,0 +1,13 @@
+package com.javacourse.project.hibernateAndJpa.dataAccess;
+
+import java.util.List;
+
+import com.javacourse.project.hibernateAndJpa.entities.City;
+
+public interface ICityDal {
+	List<City> getAll();
+	void add(City city);
+	void update(City city);
+	void delete(City city);
+	City getById(int id);
+}
